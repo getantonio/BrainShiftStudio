1ddee13df7eaa180a60d9c9ed23a79eba3fa7bcc
Antonio <getantonio@gmail.com>
2024-12-23 20:54:48 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/BrainShiftStudio' : '',
+  basePath: '/BrainShiftStudio',
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/BrainShiftStudio/' : '',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
